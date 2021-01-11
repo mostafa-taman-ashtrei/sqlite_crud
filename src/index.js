2a54c8b0aa948922b41d273a7d30aa667471edfe
NEO <neo.one.mail@gmail.com>
2021-01-11 22:51:35 +0200

    set up express && eslint

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+    app.use(morgan('combined', { stream: accessLogStream }));
+}
+
+app.get('/', (_, res) => res.json({ msg: 'ok' }));
+
+app.listen(port, () => console.log(`Server is running on port ${port}...`));
