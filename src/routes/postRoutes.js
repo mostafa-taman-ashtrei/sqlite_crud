d6e6fdd732c7adbb7c99ae1da7923911db9d0d37
NEO <neo.one.mail@gmail.com>
2021-01-12 02:10:36 +0200

    add update && delete routes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -28,4 +28,35 @@ router.post('/create', async (req, res) => {
     }
 });
 
+router.post('/update', async (req, res) => {
+    const { id, title, body } = req.body;
+
+    if (id === '') return res.status(400).json({ Error: 'The id is required' });
+    if (title === '') return res.status(400).json({ Error: 'The title is required' });
+    if (body === '') return res.status(400).json({ Error: 'The body is required' });
+
+    try {
+        const newPost = await conn('posts').where('id', id).update({ title, body });
+        if (newPost === 0) return res.status(404).json({ msg: 'Post not found ...' });
+        return res.status(200).json({ msg: 'Post updated' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ Error: 'A Server error occured ...' });
+    }
+});
+
+router.post('/del', async (req, res) => {
+    const { id } = req.body;
+    if (id === '') return res.status(400).json({ Error: 'The id is required' });
+
+    try {
+        const delPost = await conn('posts').where('id', id).del();
+        if (delPost === 0) return res.status(404).json({ msg: 'Post not found ...' });
+        return res.status(200).json({ msg: 'Post deleted ...' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ Error: 'A Server error occured ...' });
+    }
+});
+
 module.exports = router;
